8dd29f13fe22d07140001b8067cfe6a6b1907939
Evan-Pei <evan@froresystems.com>
2026-08-06 16:22:11 +0800

    feat: Add OAuth configuration settings to Config class

--- config.py
+++ config.py
@@ -18,3 +18,14 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REMEMBER_COOKIE_DURATION = timedelta(days=7)
+
+    # OAuth configuration (optional)
+    OAUTH_ENABLED = os.environ.get("OAUTH_ENABLED", "false").lower() == "true"
+    OAUTH_PROVIDER = os.environ.get("OAUTH_PROVIDER", "frore")
+    OAUTH_CLIENT_ID = os.environ.get("OAUTH_CLIENT_ID", "")
+    OAUTH_CLIENT_SECRET = os.environ.get("OAUTH_CLIENT_SECRET", "")
+    OAUTH_AUTHORIZE_URL = os.environ.get("OAUTH_AUTHORIZE_URL", "")
+    OAUTH_TOKEN_URL = os.environ.get("OAUTH_TOKEN_URL", "")
+    OAUTH_USERINFO_URL = os.environ.get("OAUTH_USERINFO_URL", "")
+    OAUTH_REDIRECT_URI = os.environ.get("OAUTH_REDIRECT_URI", "")
+    OAUTH_SCOPE = os.environ.get("OAUTH_SCOPE", "openid email profile")
